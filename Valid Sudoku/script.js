35fc2d8d82aca5c9f2f812b8a84190e0463b7486
Sazzad Hossain <148328375+shawon987543@users.noreply.github.com>
2025-11-18 00:40:39 +0600

    Add files via upload

---
+++ Valid Sudoku/script.js
@@ -0,0 +1,27 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    let rows = Array.from({length: 9}, () => new Set());
+    let cols = Array.from({length: 9}, () => new Set());
+    let boxes = Array.from({length: 9}, () => new Set());
+
+    for (let r = 0; r < 9; r++) {
+        for(let c = 0; c < 9; c++) {
+            let val = board[r][c];
+
+            if (val === ".") continue;
+            if (rows[r].has(val)) return false;
+            rows[r].add(val);
+
+            if (cols[c].has(val)) return false;
+            cols[c].add(val);
+
+            let boxIndex = Math.floor(r / 3) * 3 + Math. floor (c / 3);
+            if (boxes[boxIndex].has(val)) return false;
+            boxes[boxIndex].add(val);
+        }
+    }
+    return true;
+};
